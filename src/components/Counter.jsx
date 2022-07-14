f04258f7b4dcf8c1ffc73a4b27d3c2d802e1cd94
Сергей Субботин <zarodiny@yandex.ru>
2022-07-14 19:42:02 +1000

    3434

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { ref, set, onValue, getDatabase } from "firebase/database";
+import { useAuth } from '../hooks/use-auth'
 
 const Button = styled.button`
     min-width: 100px;
@@ -13,12 +15,32 @@ const Button = styled.button`
 `;
 
 export default function Counter() {
-    const [value, setValue] = useState(1);
+    const [value, setValue] = useState(0);
+    const { id, email } = useAuth();
+
+    console.log(value);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Отправлена форма.');
+        const writeUserData = (value, email) => {
+            const db = getDatabase();
+            set(ref(db, 'users/user' + id), {
+                counter: value,
+                email: email,
+            });
+        }
+
+        writeUserData(value, email)
+    }
     return (
         <>
-            <Button disabled={value === 1} onClick={() => setValue(value - 1)}>-</Button>
+            <Button disabled={value === 0} onClick={() => setValue(value - 1)}>-</Button>
             <span> {value} </span>
             <Button onClick={() => setValue(Number(value) + 1)}>+</Button>
+            <form onSubmit={handleSubmit}>
+                <button type="submit">Отправить</button>
+            </form>
         </>
     );
 }
